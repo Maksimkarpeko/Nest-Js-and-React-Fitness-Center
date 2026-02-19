1c567b00f13ccaa571c710d1be62e3887ad7b723
Maksim <maksim_karpe2ko@mail.ru>
2026-02-20 00:08:37 +0300

    init:added migration

---
+++ prisma/migrations/20260219210749_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateEnum
+CREATE TYPE "GlobalStatus" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateEnum
+CREATE TYPE "HallStatus" AS ENUM ('ACTIVE', 'BUSY');
+
+-- CreateEnum
+CREATE TYPE "VisitingStatus" AS ENUM ('VISITED', 'NOT_VISITED');
